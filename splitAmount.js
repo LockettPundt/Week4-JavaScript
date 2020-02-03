c4961d2d85bc13ebb1a304932152108f62ae170a
Lockett Pundt <kalifornium@hotmail.com>
2020-02-03 15:01:30 -0500

    returns your portion of a bill based on service, amount and how many people in yorur party.

---
+++ splitAmount.js
@@ -0,0 +1,26 @@
+function splitAmount() {
+    let bill = parseFloat(prompt("Enter the bill amount."));
+    let service = prompt("How was your service good, fair, bad?").toLowerCase();
+    let people = parseInt(prompt("How many people are in your party?"));
+    let total = 0;
+    while (people === 0) {
+        alert('Sounds like an existential issue...');
+        let people = parseInt(prompt("How many people are in your party?"));
+    }
+    switch (service) {
+        case "good":
+            total += (bill * 0.20) + bill;
+            break;
+        case "fair":
+            total += (bill * 0.15) + bill;
+            break;
+        case "bad":
+            total += (bill * 0.10) + bill;
+            break;
+        
+    }
+    total /= people;
+    return `Your total is $${total.toFixed(2)} including tip for ${service} service, split equally between ${people} people.`
+}
+
+console.log(splitAmount());
