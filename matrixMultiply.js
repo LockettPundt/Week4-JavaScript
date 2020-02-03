b687cbaf5aa32f4c94335aa2c46add10a5aae291
Lockett Pundt <kalifornium@hotmail.com>
2020-02-03 17:31:44 -0500

    not sure this works fully but it should multiply 2 matricies together.

---
+++ matrixMultiply.js
@@ -0,0 +1,17 @@
+// takes 2, 2 dimensional arrays and adds them together.
+
+function matrixMultiply(arr1, arr2) {
+    let sum = [];
+    for (let i = 0; i < arr1.length; i++) {
+        let temp = []
+        for (let j = 0; j <arr1[i].length;j++ ) {
+            temp.push(arr1[i][j] * arr2[i][j])
+        }
+        sum.push(temp);
+    }
+    return sum;
+}
+let a = [[1, 2], [11, 6]];
+let b = [[4, 6], [25,5]];
+console.log(matrixMultiply(a, b));
+console.log(matrixMultiply([[1, 3], [2, 4]], [[5, 2], [1, 0]]))
